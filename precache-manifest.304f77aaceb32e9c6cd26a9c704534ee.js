acb363d20e144ffa586a789fb140a6d104eb1fc1
zakizaim75 <79494228+zakizaim75@users.noreply.github.com>
2021-04-20 20:22:34 +0800

    Updates

---
+++ precache-manifest.304f77aaceb32e9c6cd26a9c704534ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9e8c891ee8c4b625f8cb974abf3c57",
+    "url": "/covid19-react-app/index.html"
+  },
+  {
+    "revision": "aa7afd8d07791f6aeef6",
+    "url": "/covid19-react-app/static/css/2.63a72fe7.chunk.css"
+  },
+  {
+    "revision": "efc78cd2045c236e8562",
+    "url": "/covid19-react-app/static/css/main.47ad8749.chunk.css"
+  },
+  {
+    "revision": "aa7afd8d07791f6aeef6",
+    "url": "/covid19-react-app/static/js/2.55f11967.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19-react-app/static/js/2.55f11967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc78cd2045c236e8562",
+    "url": "/covid19-react-app/static/js/main.1baaf617.chunk.js"
+  },
+  {
+    "revision": "51f1ac4ba0a75b7dec14",
+    "url": "/covid19-react-app/static/js/runtime-main.b101ae2a.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/covid19-react-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/covid19-react-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/covid19-react-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/covid19-react-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/covid19-react-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
